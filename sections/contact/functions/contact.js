73d886993c1b8d9471c95d5f3cf0b4661b8d5cbe
Mori Diakite <moridiakite@hotmail.com>
2019-10-26 15:12:36 +0200

    added serverless functionality to the form

--- sections/contact/functions/contact.js
+++ sections/contact/functions/contact.js
@@ -1,7 +1,36 @@
+require("dotenv").config();
+
 exports.handler = (event, _context, callback) => {
-	console.log({ event });
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({ boop: true })
+	const mailgun = require("mailgun-js");
+
+	const mg = mailgun({
+		apiKey: process.env.MAILGUN_API_KEY,
+		domain: process.env.MAILGUN_DOMAIN
+	});
+
+	//debug issue related to sending email. All the information is succesfully passed, but both error & response remains undefined)
+
+	console.log("MG INSTANCE :: ", mg);
+
+	const data = JSON.parse(event.body);
+
+	console.log("data :: ", data);
+
+	const email = {
+		from: "Excited User <me@samples.mailgun.org>",
+		to: `${data.name} <${data.email}>`,
+		subject: data.subject,
+		text: data.body
+	};
+
+	console.log("email ::", email);
+
+	mg.messages().send(email, (error, response) => {
+		console.log("ERROR :: ", error);
+		console.log("RESPONSE :: ", response);
+		callback(error, {
+			statusCode: 200,
+			body: JSON.stringify(response)
+		});
 	});
 };
